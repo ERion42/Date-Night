65485fb26ddb123c386c5d5d9bd0ca3588da8407
Socks <patricksutcliffe123@gmail.com>
2021-09-28 15:06:18 -0400

    enclosed each fetch within a function for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-var APIkeySpoon = '9d755124a4d9499396c84da0494eeff6'
-
-
+function foodRequest (){
 fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random", {
 	"method": "GET",
 	"headers": {
@@ -10,4 +8,18 @@ fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/rando
 })
 .then(response => response.json())
 .then(data => console.log(data));
+}
 
+function movieRequest (){
+fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=prime&type=movie", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+		"x-rapidapi-key": "6a33845cedmshfe3c200548f27bfp1afb9ejsne55a61a48c4b"
+	}
+})
+.then(response => response.json())
+.then(data => console.log(data))
+
+// 4: musical, 12: Adventure, 14: fantasy, 18: Drama, 27: Horror, 28: Action, 35: Comedy, 80: Crime, 878: Science fiction, 10749: Romance.
+}
