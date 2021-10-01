b2be6fb861b055b2fa6c1491e1414e6586ea4861
Matt Gomez <soraticat@gmail.com>
2021-09-30 21:01:12 -0400

    pulling another branch for updated logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// 'https://enigmatic-citadel-24557.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&key=AIzaSyC8ckXor6hYs94Ot5UefJcP4kyMtrf-578'
+// 'https://enigmatic-citadel-24557.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&key=AIzaSyC8ckXor6hYs94Ot5UefJcP4kyMtrf-578';
 
 //Declaring global variables for the Google Maps API
 var mapsAPIKeySuffix = '&key=AIzaSyC8ckXor6hYs94Ot5UefJcP4kyMtrf-578';
@@ -10,8 +10,11 @@ var lng;
 var latlng;
 var geocodeUrl;
 var spaceEncode = '%20';
-var allTypes = '&amusement_park%20campground%20park%20%20zoo%20aquarium%20art_gallery%20bowling_alley%20casino%20library%20museum%20tourist_attraction%20bar%20casino%20night_club%20bakery%20cafe%20restaurant%20movie_theater%20'
+var commaEncode = '%2C';
+var allTypes = '&amusement_park%20campground%20park%20%20zoo%20aquarium%20art_gallery%20bowling_alley%20casino%20library%20museum%20tourist_attraction%20bar%20casino%20night_club%20bakery%20cafe%20restaurant%20movie_theater%20';
 //var locationAndRadius = '&location=' + lat + '%20' + lng + '&radius=15000&types='
+
+'https://enigmatic-citadel-24557.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&fields=name%20type&locationbias=circle:15000@' + latlng + '&inputtype=textquery&key=AIzaSyC8ckXor6hYs94Ot5UefJcP4kyMtrf-578';
 
 var searchTypes = function(){
     /*
@@ -53,7 +56,6 @@ var geocodeAddress = function(address){
 
     var addressArr = address.split(' ');
     var addressEncoded = addressArr.join('%20');
-    var geocodeResults = new google.maps.Geocoder();
     geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + addressEncoded + mapsAPIKeySuffix;
     console.log(geocodeUrl);
 
@@ -64,26 +66,27 @@ var geocodeAddress = function(address){
        console.log(lat);
        console.log(lng);
         var latlng = lat + '%2C' + lng;
-        console.log(latlng);        
-   }).then(searchNearby(latlng));
+        localStorage.setItem('location', latlng);
+   }).then(searchNearby());
 
 };
-
-console.log(latlng);
+https://enigmatic-citadel-24557.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbySearch/json?&location=33.7489954.-84.3879824&radius=15000&key=AIzaSyC8ckXor6hYs94Ot5UefJcP4kyMtrf-578
 
 //Event listener for the address form submit button. Calls geocodeAddress function.
 $('form').submit(function(event){
     event.preventDefault();
     var address = ($('#address').val());
     geocodeAddress(address);
-    // searchNearby(latlng);
-    // console.log(latlng);
 });
 
+var initMap = function (){
 
+}
 //Test fetch for google maps data. This test uses the cors anywhere address prepended to the url in order to avoid triggering cors due to the local host address. The test worked. 
 var searchNearby = function(){
-    var mapsAPIUrl = 'https://enigmatic-citadel-24557.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&location=' + latlng + '&fields=name%2Ctype%2Copening_hours%2Cprice_level%2Crating' + allTypes + '&key=AIzaSyC8ckXor6hYs94Ot5UefJcP4kyMtrf-578'
+    latlng = localStorage.getItem('location', latlng);
+    console.log(latlng);
+    var mapsAPIUrl = 'https://enigmatic-citadel-24557.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=name%2Ctype&locationbias=circle:15000@' + latlng + '&inputtype=textquery&key=AIzaSyC8ckXor6hYs94Ot5UefJcP4kyMtrf-578';
     // mapsAPIUrlPrefix + mapsAPIKeySuffix;
     console.log(mapsAPIUrl);
     fetch(mapsAPIUrl).then(function(response){
